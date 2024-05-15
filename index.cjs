8ab3bf519efe4271767beef6640af70ab0013b07
pedroorf <pedroorf@gmail.com>
2024-05-15 13:05:28 -0300

    wip

--- index.cjs
+++ index.cjs
@@ -1,9 +1,11 @@
-// const Multipart = require("lambda-multipart");
+const Multipart = require("lambda-multipart");
 // const AWS = require("aws-sdk");
-import Multipart from "lambda-multipart";
+// import Multipart from "lambda-multipart";
 //import AWS from "aws-sdk";
-import * as AWS from "@aws-sdk/client-s3"
-import { v4 as uuidv4 } from 'uuid'
+// import * as AWS from "@aws-sdk/client-s3"
+const AWS = requre("@aws-sdk/client-s3")
+// import { v4 as uuidv4 } from 'uuid'
+const { v4: uuidv4 } = require('uuid');
 
 const s3 = new AWS.S3();
 
